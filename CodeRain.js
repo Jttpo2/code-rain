a5b3d26b972b6398a642eb28bafb303f58333718
John Petersson <johnpetersson@gmail.com>
2017-02-24 13:37:19 +0100

    Seeing tings

--- CodeRain.js
+++ CodeRain.js
@@ -1,9 +1,37 @@
+var symbol;
+
 function setup() {
+	createCanvas(
+		window.innerWidth, 
+		window.innerHeight);
 	background(0);
-	createCanvas(600,400);
+
+	symbol = new Symbol(width/2, height/2);
+	symbol.setToRandomSymbol();
 }
 
 function draw() {
-	  ellipse(50, 50, 80, 80);
+	  symbol.render();
+}
+
+function Symbol(x, y) {
+	this.x = x;
+	this.y = y;
+	this.value;
+
+	this.setToRandomSymbol = function() {
+		this.value = String.fromCharCode(
+			0x30A0 + round(random(0,96))
+			);
+
+	}
+
+	this.render = function() {
+		fill(0, 255, 70);
+		text(this.value, this.x, this.y);
+	}
+}
+
+function stream() {
 
 }
