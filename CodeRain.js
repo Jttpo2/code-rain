4ea8a2f4455a88af6d96c627b2f635624799c061
John Petersson <johnpetersson@gmail.com>
2017-02-24 13:49:58 +0100

    Raining shifting symbol

--- CodeRain.js
+++ CodeRain.js
@@ -1,4 +1,5 @@
 var symbol;
+var symbolSize = 60;
 
 function setup() {
 	createCanvas(
@@ -6,18 +7,25 @@ function setup() {
 		window.innerHeight);
 	background(0);
 
-	symbol = new Symbol(width/2, height/2);
+	symbol = new Symbol(
+		width/2, 
+		0,
+		random(5, 10));
 	symbol.setToRandomSymbol();
+	textSize(symbolSize);
 }
 
 function draw() {
-	  symbol.render();
+	background(0);
+	symbol.render();
 }
 
-function Symbol(x, y) {
+function Symbol(x, y, speed) {
 	this.x = x;
 	this.y = y;
 	this.value;
+	this.speed = speed;
+	this.switchInterval = round(random(2, 20));
 
 	this.setToRandomSymbol = function() {
 		this.value = String.fromCharCode(
@@ -29,9 +37,18 @@ function Symbol(x, y) {
 	this.render = function() {
 		fill(0, 255, 70);
 		text(this.value, this.x, this.y);
+		this.rain();
+		if (frameCount % this.switchInterval == 0) {
+			this.setToRandomSymbol();	
+		}
+	}
+
+	this.rain = function() {
+		this.y = (this.y >= height) ? 0 : this.y += this.speed;
+
 	}
 }
 
-function stream() {
-
+function Stream() {
+	
 }
