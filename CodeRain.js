c7aad665f7435de49bf45add286498af29b70ce3
John Petersson <johnpetersson@gmail.com>
2017-02-24 16:11:01 +0100

    StreamHandler. Memory leak sorted.

--- CodeRain.js
+++ CodeRain.js
@@ -1,5 +1,7 @@
 var symbolSize = 13;
-var streams = [];
+var streamMinStartPositionY = -1000;
+
+var streamHandler;
 
 function setup() {
 	createCanvas(
@@ -7,27 +9,16 @@ function setup() {
 		window.innerHeight);
 	background(0);
 
-
-	let x = 0;
-	for (var i=0; i<=width/symbolSize; i++) {
-		stream = new Stream();
-		stream.generateSymbols(
-			x, 
-			random(-1000, 0)
-			);	
-		streams.push(stream);
-		x += symbolSize;
-	}
-
+	streamHandler = new StreamHandler();
+	streamHandler.generateStreams();
+	
 	textSize(symbolSize);
 }
 
 function draw() {
 	background(0, 150);
-	streams.forEach(function(stream) {
-		stream.render();	
-	});
-	
+
+	streamHandler.render();
 }
 
 function Symbol(x, y, speed, isShiner, alpha) {
@@ -43,7 +34,6 @@ function Symbol(x, y, speed, isShiner, alpha) {
 		this.value = String.fromCharCode(
 			0x30A0 + round(random(0,96))
 			);
-
 	}
 
 	this.render = function() {
@@ -57,13 +47,14 @@ function Symbol(x, y, speed, isShiner, alpha) {
 		this.rain();
 		if (frameCount % this.switchInterval == 0) {
 			this.setToRandomSymbol();
+			// New random interval till next switch
 			this.switchInterval = round(random(2, 20));	
 		}
 	}
 
 	this.rain = function() {
-		this.y = (this.y >= height*1.5) ? 0 : this.y += this.speed;
-
+		// this.y = (this.y >= height*1.5) ? 0 : this.y += this.speed;
+		this.y += this.speed;
 	}
 }
 
@@ -73,10 +64,11 @@ function Stream() {
 	this.speed = random(5, 20);
 
 	this.generateSymbols = function(x, y) {
+		this.symbols.length = 0;
 		let isShiner = round(random(0, 4)) == 1;
 		for (var i=0; i<=this.totalSymbols; i++) {
 			// Fade with position in stream
-			let alpha = map(i, 0, this.totalSymbols, 255, 90);
+			let alpha = map(i, 0, this.totalSymbols, 180, 70);
 			symbol = new Symbol(x, y, this.speed, isShiner, alpha);
 			symbol.setToRandomSymbol();
 			this.symbols.push(symbol);
@@ -89,5 +81,37 @@ function Stream() {
 		this.symbols.forEach(function(symbol) {
 			 symbol.render();
 			});
+	}
+
+	this.isGoneFromView = function() {
+		return this.symbols[this.symbols.length-1].y > height;
+	}
+}
+
+function StreamHandler() {
+	this.streams = [];
+
+	this.generateStreams = function() {
+		this.streams.length = 0;
+		let x = 0;
+		for (var i=0; i<=width/symbolSize; i++) {
+			stream = new Stream();
+			stream.generateSymbols(
+				x, 
+				random(streamMinStartPositionY, 0)
+				);	
+			this.streams.push(stream);
+			x += symbolSize;
+		}
+	}
+
+	this.render = function() {
+		this.streams.forEach(function(stream) {
+			if (stream.isGoneFromView()) {
+				console.log('gone ' + stream.symbols[0].x);
+				stream.generateSymbols(stream.symbols[0].x, streamMinStartPositionY);
+			}
+			stream.render();	
+		});
 	}
 }
